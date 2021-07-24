372f5a55adcd60f4f3069adcea0af75da0bfc4a9
Ivan Popovic <ivanivopop@gmail.com>
2021-07-24 21:57:25 +0200

    employee-start-page

--- employee/urls.py
+++ employee/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.EmployeeView.as_view(),name="employee")
+    path("",views.EmployeeView.as_view(),name="employee"),
+    path("<slug:slug>",views.UserView.as_view(),name="employee-info")
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,8 +1,20 @@
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 class EmployeeView(View):
+
+    @method_decorator(login_required(login_url='/'))
     def get(self, request):
-        return render(request,"employee/employee.html")
+        user = request.user
+        return render(request,"employee/employee.html",{
+            "user":user
+        })
+
+class UserView(View):
+    pass
